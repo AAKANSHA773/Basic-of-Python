2a01bfdd85c1832b961f5a2bc6a9eb4409f05d88
aakansha panwar <60297202+AAKANSHA773@users.noreply.github.com>
2021-05-25 19:27:45 +0530

    Create varibles data type.py

---
+++ varibles data type.py
@@ -0,0 +1,42 @@
+print("python is eassy !!")
+a=13
+b=100
+c=-66 #int type of variable
+print(a,b,c)
+print(type(a)) #this line show data type of variable.
+
+x=33.5
+y=25.5
+z=205.0 #flaot type of variable
+print(x,y,z)
+print(type(x))
+
+d=3+5j #complex type of variable
+print(d)
+print(type(d))
+
+e=0B1010
+print(e) #int type of variable write in binary 
+print(type(e))
+
+f=0xFF #int type of variable write in hexadecimal
+print(f)
+print(type(f))
+
+g=True #bool type of varible
+print(g)
+print(type(g))
+
+h=int(x)
+print(h)
+print(type(h))
+
+i=float("34.5")
+print(i)
+print(type(i))
+
+print(bin(10))#print in binary number.
+#output 0b is code of binary number after that 10 converted in binary - 0b1010
+print(hex(25)) # print in hexadecimal nnumber
+#output 0x is code of binary number after that 25 converted in binary - 0x19
+
